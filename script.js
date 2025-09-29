7f8aa85a486c4c65db9f3f3e57c3a3d341dffefb
yamina-003 <yaminabentaifour@gmail.com>
2025-09-29 15:30:46 +0100

    Modify the create grid logic

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 const container = document.querySelector(".container")
 
-for (let i = 0; i< 256; i++){
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
-    
+
+function createGrid(n){
+
+    const containerWidth = container.clientWidth;
+    const gap = 8;
+    const availabeSpace = containerWidth - (n -1)* gap;
+    const flexBasis = availabeSpace / n;
+
+    for ( i = 0; i < n * n; i++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.flex = ` 1 1 ${flexBasis}px`;
+            container.appendChild(square);
+        }
+
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => square.addEventListener("mouseenter", ()=> square.style.backgroundColor = "white"));
+
 }
 
-const squares = document.querySelectorAll(".square");
-squares.forEach(square =>{
-    square.addEventListener("mouseenter", ()=> square.style.backgroundColor = "white");
-    square.addEventListener("mouseleave", ()=> square.style.backgroundColor = "black");
-})
+createGrid(16);
+
+
+
+
+
+
 
 
 
